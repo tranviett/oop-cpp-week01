29b049ec31eb27478013b2565e1584a10ade7781
tranviett <tranviett27@gmail.com>
2026-08-20 22:11:26 +0700

    Update main.cpp

--- buoi1/main.cpp
+++ buoi1/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-int main() {
-	std::cout << "Hello, world!\n";
-	return 0;
+int main()
+{
+	  std::cout <<"==========================\n";
+	  std::cout << "     MY FIRST C++ APP     \n";	  
+	  std::cout << "==========================\n";
+	  std::cout << "Name:Tran Duc Viet\n";
+	  std::cout << "Course: OOP with C++\n";
+	  std::cout << "I'm ready to learn C++\n";
+	  return 0;
 }
